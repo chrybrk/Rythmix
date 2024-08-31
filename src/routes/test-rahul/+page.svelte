786f48b1b2562cd3dc5eb982ec157bf4d0e8a857
chrybrk <rahul003chry@gmail.com>
2024-08-31 20:10:41 +0530

    random ui elements

--- src/routes/test-rahul/+page.svelte
+++ src/routes/test-rahul/+page.svelte
@@ -1,8 +1,42 @@
+<script>
+</script>
+
 <svelte:head>
 	<title>Test</title>
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<div>
-	Test of rahul
+<div class="card">
+	<div class="container">
+		<div class="title">Test container</div>
+		<div class="content">
+			Some few text.
+		</div>
+	</div>
 </div>
+
+<style>
+	.card {
+		width: 200px;
+		height: 200px;
+		margin: 0 auto;
+		padding: 10rm;
+		background-color: powderblue;
+		border: 0px;
+		border-radius: 18px;
+		position: relative;
+	}
+
+	.container {
+		position: absolute;
+	}
+
+	.container .title {
+		font-size: 24px;
+		margin: 10px;
+	}
+
+	.container .content {
+		margin: 10px;
+	}
+</style>
